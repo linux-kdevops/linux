891f6b61f629ad13efc22d7f0c2ce980632f33d5
Luis Chamberlain <mcgrof@kernel.org>
2023-06-09 03:43:06 -0700

    nvme: enable LBS support
    Now that the block device cache allows LBS, and we have buffered-io
    support, and we support both buffer-heads and iomap aops on the
    block device cache through the inode dynamically, enable support for
    LBA formats > PAGE_SIZE.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -1997,9 +1997,6 @@ static bool nvme_lba_shift_supported(struct nvme_ns_head *head,
 	if (head->lba_shift <= PAGE_SHIFT)
 		return true;
 
-	if (IS_ENABLED(CONFIG_BUFFER_HEAD))
-		return false;
-
 	if (head->lba_shift <= NVME_MAX_SHIFT_SUPPORTED)
 		return true;
 
