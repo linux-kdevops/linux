461f78958cf293e2a3a4b4f099d3ac180ed42532
Luis Chamberlain <mcgrof@kernel.org>
2023-09-03 12:13:12 -0700

    bdev: simplify coexistance
    Now that we have a bdev inode aops easily switch between buffer-heads
    and iomap we can simplify our requirement. This also enables usage
    of xfs on devices which for example have a physical block size greater
    than page size but the logical block size is only 4k or lower.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- block/bdev.c
+++ block/bdev.c
@@ -156,13 +156,6 @@ static void set_init_blocksize(struct block_device *bdev)
 	}
 }
 
-static int bdev_bsize_limit(struct block_device *bdev)
-{
-	if (BD_INODE(bdev)->i_data.a_ops == &def_blk_aops_iomap)
-		return 1 << (PAGE_SHIFT + MAX_PAGECACHE_ORDER);
-	return PAGE_SIZE;
-}
-
 #ifdef CONFIG_BUFFER_HEAD
 static void bdev_aops_set(struct block_device *bdev,
 			  const struct address_space_operations *aops)
@@ -220,9 +213,10 @@ int set_blocksize(struct file *file, int size)
 {
 	struct inode *inode = file->f_mapping->host;
 	struct block_device *bdev = I_BDEV(inode);
+	unsigned int bdev_size_limit = 1 << (PAGE_SHIFT + MAX_PAGECACHE_ORDER);
 
 	/* Size must be a power of two, and between 512 and supported order */
-	if (size > bdev_bsize_limit(bdev) || size < 512 || !is_power_of_2(size))
+	if (size > bdev_size_limit || size < 512 || !is_power_of_2(size))
 		return -EINVAL;
 
 	/* Size cannot be smaller than the size supported by the device */
