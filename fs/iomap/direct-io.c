0499e2e4fb269fc309dbd96fc83132883bdb79f0
Luis Chamberlain <mcgrof@kernel.org>
2025-02-28 00:12:13 +0000

    iomap: use BLK_MAX_BLOCK_SIZE for the iomap zero page
    There is no point in modifying two locations now that we have
    a sensible BLK_MAX_BLOCK_SIZE defined which is only lifted once
    we have tested and validated things.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- fs/iomap/direct-io.c
+++ fs/iomap/direct-io.c
@@ -30,7 +30,7 @@
 /*
  * Used for sub block zeroing in iomap_dio_zero()
  */
-#define IOMAP_ZERO_PAGE_SIZE (SZ_64K)
+#define IOMAP_ZERO_PAGE_SIZE (BLK_MAX_BLOCK_SIZE)
 #define IOMAP_ZERO_PAGE_ORDER (get_order(IOMAP_ZERO_PAGE_SIZE))
 static struct page *zero_page;
 
