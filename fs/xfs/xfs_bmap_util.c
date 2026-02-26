50293eca3121c4ec5ef27b87757e7656d5302cdf
Pankaj Raghav <p.raghav@samsung.com>
2026-02-26 14:25:33 +0100

    xfs: add flags field to xfs_alloc_file_space
    Currently, xfs_alloc_file_space() hardcodes the XFS_BMAPI_PREALLOC flag
    when calling xfs_bmapi_write(). This restricts its capability to only
    allocating unwritten extents.
    
    In preparation for adding FALLOC_FL_WRITE_ZEROES support, which needs to
    allocate space and simultaneously convert it to written and zeroed
    extents, introduce a 'flags' parameter to xfs_alloc_file_space(). This
    allows callers to explicitly pass the required XFS_BMAPI_* allocation
    flags.
    
    Update all existing callers to pass XFS_BMAPI_PREALLOC to maintain the
    current behavior. No functional changes intended.
    
    Signed-off-by: Pankaj Raghav <p.raghav@samsung.com>

--- fs/xfs/xfs_bmap_util.c
+++ fs/xfs/xfs_bmap_util.c
@@ -646,7 +646,8 @@ int
 xfs_alloc_file_space(
 	struct xfs_inode	*ip,
 	xfs_off_t		offset,
-	xfs_off_t		len)
+	xfs_off_t		len,
+	uint32_t flags)
 {
 	xfs_mount_t		*mp = ip->i_mount;
 	xfs_off_t		count;
@@ -748,7 +749,7 @@ xfs_alloc_file_space(
 		 * will eventually reach the requested range.
 		 */
 		error = xfs_bmapi_write(tp, ip, startoffset_fsb,
-				allocatesize_fsb, XFS_BMAPI_PREALLOC, 0, imapp,
+				allocatesize_fsb, flags, 0, imapp,
 				&nimaps);
 		if (error) {
 			if (error != -ENOSR)
